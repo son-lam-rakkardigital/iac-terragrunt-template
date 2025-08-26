2a78a9dde084abcb0e5e8a4f567ce248fd7dd7e2
son.lam <son.lam@rakkardigital.com>
2025-08-26 09:13:13 +0700

    style: format code for consistency in S3 bucket test

--- modules/s3_buckets/test/s3_buckets_test.go
+++ modules/s3_buckets/test/s3_buckets_test.go
@@ -1,41 +1,40 @@
 package test
 
 import (
-    "strings"
-    "testing"
+	"strings"
+	"testing"
 
-    "github.com/gruntwork-io/terratest/modules/aws"
-    "github.com/gruntwork-io/terratest/modules/terraform"
-    "github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestS3BucketVersioning(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-    awsRegion := "us-east-1"
-    environment := "test"
-    bucketPrefix := "terratest-bucket"
+	awsRegion := "ap-southeast-1"
+	environment := "test"
+	bucketPrefix := "terratest-bucket"
 
-    terraformOptions := &terraform.Options{
-        TerraformDir: "../",
-        Vars: map[string]interface{}{
-            "environment":        environment,
-            "aws_region":         awsRegion,
-            "bucket_name_prefix": bucketPrefix,
-            "enable_versioning":  true,
-        },
-    }
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+		Vars: map[string]interface{}{
+			"environment":        environment,
+			"aws_region":         awsRegion,
+			"bucket_name_prefix": bucketPrefix,
+			"enable_versioning":  true,
+		},
+	}
 
-    defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
-    terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
-    bucketName := terraform.Output(t, terraformOptions, "bucket_name")
+	bucketName := terraform.Output(t, terraformOptions, "bucket_name")
 
-    expectedPrefix := bucketPrefix + "-" + environment + "-" + awsRegion
-    assert.True(t, strings.HasPrefix(bucketName, expectedPrefix))
+	expectedPrefix := bucketPrefix + "-" + environment + "-" + awsRegion
+	assert.True(t, strings.HasPrefix(bucketName, expectedPrefix))
 
-    versioning := aws.GetS3BucketVersioning(t, awsRegion, bucketName)
-    assert.Equal(t, "Enabled", versioning)
+	versioning := aws.GetS3BucketVersioning(t, awsRegion, bucketName)
+	assert.Equal(t, "Enabled", versioning)
 }
-
